6001f09408f9729619c36a911c4e5b4a2690557e
Lucian Cojocar <cojocar@users.noreply.github.com>
2020-03-28 17:39:08 -0700

    headless: TelemetryPipe: update comments

--- src/telemStream/TelemetryPipe.java
+++ src/telemStream/TelemetryPipe.java
@@ -15,6 +15,10 @@ public class TelemetryPipe implements Runnable {
 	String pipePath;
 	int trackedSatId = -1;
 	FileOutputStream out = null;
+
+	/* Named pipe should exist, this code path does not take care of creating
+	 * the pipe. Use mkfifo (on linux) to create such a pipe.
+	 */
 	public TelemetryPipe (int trackedSatId, String pipeName) {
 		this.pipePath = getArchPipeNamePath(pipeName);
 		this.trackedSatId = trackedSatId;
@@ -25,8 +29,8 @@ public class TelemetryPipe implements Runnable {
 		if (os.isWindows()) {
 			return "\\\\.\\pipe\\" + pipeName;
 		}
-		// TODO: not sure what to do on MAC
-		// on Linux, we can use the direct name
+		// TODO: not sure what to do on MAC.
+		// on Linux, we can use the direct name as a path
 		return pipeName;
 	}
 
@@ -38,7 +42,9 @@ public class TelemetryPipe implements Runnable {
 			Log.println("TelemetryPipeThread failed to open pipe");
 			return;
 		}
+
 		Log.println("TelemetryPipeThread START");
+		Log.println("station lat, lon: " + Config.latitude + " " + Config.longitude);
 		while (true) {
 			try {
 				Thread.sleep(1000);
@@ -57,22 +63,22 @@ public class TelemetryPipe implements Runnable {
 	}
 
 	private String getPositionNow(int trackedSatId) {
+		// The output format is EASYCOMM II. A full description of this format
+		// can be found here: https://www.mustbeart.com/software/easycomm.txt
+		//
+		// We implement only a subset of commands (the AZ and EL)
 		Spacecraft sat = Config.satManager.spacecraftList.get(trackedSatId);
 		if (sat.satPos == null) {
-			return "";
+			return "ERR";
 		}
 		double az = FramePart.radToDeg(sat.satPos.getAzimuth());
 		double el = FramePart.radToDeg(sat.satPos.getElevation());
 
 		String position = "AZ" + String.format("%2.1f", az) + " EL" + String.format("%2.1f", el);
 		StringBuffer ret = new StringBuffer(position);
-		if (Config.foxTelemCalcsDoppler) {
-			double freq = sat.satPos.getDopplerFrequency(sat.user_telemetryDownlinkFreqkHz);
-			String sign="";
-			if (freq > 0) sign = "+";
-			position = position + " |" + sign+String.format("%2.3f", freq) + "kHz";
-		}
+
 		Log.println(position);
+
 		return ret.toString();
 	}
 }
